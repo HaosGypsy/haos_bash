a6ce4e45b2f50b7f2a2789aca169783f914997f1
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-03-26 14:59:37 +0000

    Added initial tool.

---
+++ haos_bash/get_spring_2016_repos.bash
@@ -0,0 +1,17 @@
+# Make git rememember passwords for 30 minutes.
+git config --global credential.helper 'cache --timeout=1800'
+
+# Get the toolset.
+git clone https://gitlab.com/ewha_spring_2016/z_spring_2016_bash_toolset.git
+
+# Get all the class repositories.
+git clone https://gitlab.com/ewha_spring_2016/38046_01_financial_technology.git
+
+git clone https://gitlab.com/ewha_spring_2016/36564_01_basic_electronics_engineering.git
+
+git clone https://gitlab.com/koreahaos/37269_01_smart_software_project.git
+
+git clone https://gitlab.com/ewha_spring_2016/36095_02_basic_circuit_lab.git
+
+git clone https://gitlab.com/koreahaos/30490_01_metal_and_expression.git
+
