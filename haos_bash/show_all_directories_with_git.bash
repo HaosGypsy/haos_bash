1c9263422bfca4f82f386c6e9833ae7e51400a84
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-04-13 07:26:35 +0000

    Wholesale commit...(this has got to change!)

---
+++ haos_bash/show_all_directories_with_git.bash
@@ -0,0 +1,126 @@
+function ask_for_key_press {
+    local user_input
+    read -n1 -r -p "Any key to continue : " user_input
+    echo ""
+}
+
+function ask_for_key_press_allow_quit {
+    local user_input
+    read -n1 -r -p "Any key to continue (q to quit) : " user_input
+    echo ""
+    if [ "$user_input" = 'q' ]; then
+        exit;
+    fi
+}
+
+function work_with_git_directory {
+    echo "$1"
+}
+
+function populate_array {
+    count=0;
+    GIT_DIRS=()
+    while IFS= read -r -d $'\0' file; do
+      let "count++"
+      GIT_DIRS[$count]="$file"
+    done < <(find . -name .git -type d -prune -print0)
+    echo "Found $count git directories!"
+}
+
+function display_git_directories {
+    for git_directory in "${GIT_DIRS[@]}"
+    do
+      echo "${git_directory}";
+    done
+}
+
+function say_hello {
+    echo "Work with git directories!"
+    printf "Working directory is : %s\n" $PWD
+    #ask_for_key_press_allow_quit
+}
+
+function init_program {
+    count=0;
+    GIT_DIRS=();
+    CALLED_FROM_DIR="$PWD";
+}
+
+function show_options {
+    echo "hi"
+}
+
+function update_repos {
+    for git_directory in "${GIT_DIRS[@]}"
+    do
+      cd $git_directory;
+      cd ..
+      echo "$PWD";
+      
+      ls
+      
+      cd $CALLED_FROM_DIR;
+      #echo "${git_directory}";
+      ##ls
+    done
+}
+
+function add_all_commit_all_push_all {
+    for git_directory in "${GIT_DIRS[@]}"
+    do
+      cd $git_directory;
+      cd ..
+      echo "$PWD";
+      
+      git add .
+      git commit -m 'Wholesale commit...(this has got to change!)'
+      git push --all
+      ##ls
+      
+      cd $CALLED_FROM_DIR;
+      #echo "${git_directory}";
+      ##ls
+    done
+    
+}
+
+function run_program_loop {
+    populate_array
+    local users_choice
+    
+    local user_wants_to_quit="false"
+    
+    while [ "$user_wants_to_quit" != "true" ]
+    do
+        read -p "Enter your choice : " users_choice
+        printf "You choose : %s\n" $users_choice
+        
+        case $users_choice in
+            a)
+               update_repos
+               ;;
+            b)
+               add_all_commit_all_push_all
+               ;;
+            c)
+               display_git_directories
+               ;;
+            d)
+               display_git_directories
+               ;;
+            q)
+                user_wants_to_quit="true"
+                ;;
+            *)
+                echo "Do not understand input!";
+        esac
+    done
+    
+}
+
+say_hello
+init_program
+run_program_loop
+
+:<<EOF
+EOF
