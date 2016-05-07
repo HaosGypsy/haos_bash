5c445c0a964039b14a3dfe7f6982705046781c25
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-05-07 01:33:14 +0000

    WHOLESALE COMMIT - ADD COMMIT MESSAGE NEXT TIME!

--- haos_bash/produce_an_index_with_all_the_html_pages.bash
+++ haos_bash/produce_an_index_with_all_the_html_pages.bash
@@ -1,7 +1,7 @@
 # ToDo : Have it ask for user input and/or except arguements.
 
 page_title="script_created_index"
-output_file="index.html"
+output_file="index_boiler.html"
 
 cat >"$output_file"<<EOF
 <!DOCTYPE html>
