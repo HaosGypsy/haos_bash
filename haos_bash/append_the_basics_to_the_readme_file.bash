8a06198c9ddb93f2e502ad62beccfc2f8c1c6a99
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-05-10 11:19:38 +0000

    WHOLESALE COMMIT - ADD COMMIT MESSAGE NEXT TIME!

---
+++ haos_bash/append_the_basics_to_the_readme_file.bash
@@ -0,0 +1,31 @@
+:<<EOF
+This is a script for adding basic headings to readme files.
+
+There are really only six questions that can ever be asked.
+So ... let's automate that!
+
+STATUS : IN PROGRESS!
+
+ToDo : Set this up so it can be applied to ALL THE FILES!!!
+ToDo : stop being so ambitious...
+ToDo : Have it output the readme file so user knows what they did.
+ToDo : 
+EOF
+
+function make_readme() {
+
+    local text_for_file=$'';
+    
+    text_for_file+=$'\n\n**SCRIPTED READ ME APPEND!!**\n\n';
+    text_for_file+=$'#### WHO!\n\n';
+    text_for_file+=$'#### WHAT!\n\n';
+    text_for_file+=$'#### WHERE!\n\n';
+    text_for_file+=$'#### WHEN!\n\n';
+    text_for_file+=$'#### WHY!\n\n';
+    text_for_file+=$'#### HOW!\n\n';
+
+    
+    printf "%s" "$text_for_file" >> README.md
+}
+
+make_readme
