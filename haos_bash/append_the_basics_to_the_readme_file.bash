6164f02b0352c7d8a26e84a03a2f801bc60b2637
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-05-13 00:50:19 +0000

    WHOLESALE COMMIT - ADD COMMIT MESSAGE NEXT TIME!

--- haos_bash/append_the_basics_to_the_readme_file.bash
+++ haos_bash/append_the_basics_to_the_readme_file.bash
@@ -7,7 +7,7 @@ So ... let's automate that!
 STATUS : IN PROGRESS!
 
 ToDo : Set this up so it can be applied to ALL THE FILES!!!
-ToDo : stop being so ambitious...
+ToDo : Stop being so ambitious?
 ToDo : Have it output the readme file so user knows what they did.
 ToDo : 
 EOF
@@ -17,12 +17,18 @@ function make_readme() {
     local text_for_file=$'';
     
     text_for_file+=$'\n\n**SCRIPTED READ ME APPEND!!**\n\n';
-    text_for_file+=$'#### WHO!\n\n';
-    text_for_file+=$'#### WHAT!\n\n';
-    text_for_file+=$'#### WHERE!\n\n';
-    text_for_file+=$'#### WHEN!\n\n';
-    text_for_file+=$'#### WHY!\n\n';
-    text_for_file+=$'#### HOW!\n\n';
+    text_for_file+=$'#### **WHO!**\n';
+    text_for_file+=$'* *put text here!*\n';
+    text_for_file+=$'#### **WHAT!**\n';
+    text_for_file+=$'* *put text here!*\n';
+    text_for_file+=$'#### **WHERE!**\n';
+    text_for_file+=$'* *put text here!*\n';
+    text_for_file+=$'#### **WHEN!**\n';
+    text_for_file+=$'* *put text here!*\n';
+    text_for_file+=$'#### **WHY!**\n';
+    text_for_file+=$'* *put text here!*\n';
+    text_for_file+=$'#### **HOW!**\n';
+    text_for_file+=$'* *put text here!*\n';
 
     
     printf "%s" "$text_for_file" >> README.md
