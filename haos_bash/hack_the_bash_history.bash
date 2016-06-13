06ecfaf98579fb79da7a3f6b9a2ac4e144bf1ed5
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-06-13 22:16:13 +0000

    Script commit from Cloud9 VM(C9).C9 user : KoreaHaos. Committing script name :  Script committing function name : create_cloud9_manage_git_directories_script_commit.

--- haos_bash/hack_the_bash_history.bash
+++ haos_bash/hack_the_bash_history.bash
@@ -3,11 +3,13 @@ function modify_history() {
     cd ..
     
     echo "$most_imporant_history_item" > $GOPATH/tempfile
-    #echo 'line 2' >> $GOPATH/tempfile
+    echo "$line_2" >> $GOPATH/tempfile
     
     cat .bash_history >> $GOPATH/tempfile
     
     cp $GOPATH/tempfile .bash_history
+    
+    rm $GOPATH/tempfile
 }
 
 function set_scripts_path() {
@@ -22,7 +24,15 @@ set_scripts_path
 set_scripts_path_and_name
 
 most_imporant_history_item="bash $SCRIPTS_DIR/manage_git_directories.bash"
+line_2="bash $SCRIPTS_DIR/manage_git_directories.bash A"
+
 
 modify_history
+
+
+#n ToDo : Figure out how to make the new line available with terminsal restart
+# kill -9 $PPID
+
+
 
 echo "You need to close your terminal and reopen before history mods will occure."
