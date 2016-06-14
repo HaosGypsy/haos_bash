713f7e11b85b9c3c364fa81654008adeb2520d71
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-06-14 21:03:58 +0000

    Script commit from Cloud9 VM(C9).C9 user : KoreaHaos. Committing script name :  Script committing function name : create_cloud9_manage_git_directories_script_commit.

--- haos_bash/zip_up_code_to_bend.bash
+++ haos_bash/zip_up_code_to_bend.bash
@@ -4,17 +4,23 @@
 # It's not perfect, but what is?
 
 parent_folder_path="$GOPATH"
-zip_file_name="haos_smartcar.zip"
-folder_to_zip="haos_smartcar_project/"
+folder_to_zip="$parent_folder_path/a_design_for_ewha/"
+
+new_zip_file_name="a_manage_haos.zip"
+new_zip_file_path_and_name="$folder_to_zip/$new_zip_file_name"
 
 # Move to directory where sketch is stored.
-cd "parent_folder_path"
+#cd "$parent_folder_path"
 
 # Remove any prevoius zip present
-if [ -e "$zip_file_name" ]
+if [ -e "$new_zip_file_path_and_name" ]
 then
-    rm "$zip_file_name"
+    rm "$new_zip_file_path_and_name"
 fi
 
+echo 
 # Zip all the files useable in the CodeBender IDE
-zip -9 -y -r -q -j "$zip_file_name" "$folder_to_zip"/ -i '*.ino' '*.cpp' '*.h'
+# zip -9 -y -r -q -j "$zip_file_name" "$folder_to_zip"/ -i '*.ino' '*.cpp' '*.h'
+zip -9 -y -r -q -j "$new_zip_file_path_and_name" "$folder_to_zip"/ -i '*.ino' '*.cpp' '*.h'
+
+
